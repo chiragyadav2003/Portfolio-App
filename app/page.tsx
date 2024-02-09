53e86d7020c625f1cce1dcc7e0303df4112e165c
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-02-09 19:59:13 +0530

    header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Intro from "@/components/Intro"
+
 export default function Home() {
   return (
-    <main className="">
+    <main className=" flex flex-col justify-center items-center px-4">
+      <Intro/>
     </main>
   )
 }
