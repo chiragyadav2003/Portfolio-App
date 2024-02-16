3fa02103df56bcbfe6fd701a2ba93e5b7ff42f0f
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-02-17 00:50:27 +0530

    experience configuring

--- components/experience.tsx
+++ components/experience.tsx
@@ -11,16 +11,32 @@ export default function Experience() {
     id="experience"
     >
         <SectionHeading>My Experience</SectionHeading>
-        <VerticalTimeline>
+        <VerticalTimeline lineColor="">
             {
                 experiencesData.map((item,index)=>(
                     <React.Fragment key={index}>
                         <VerticalTimelineElement
                         visible={true}
+                        contentStyle={{
+                                background:"#f3f4f6",
+                                boxShadow:"none",
+                                border:"1px solid rgba(0,0,0,0.05)",
+                                textAlign:"left",
+                                padding:"1.3rem 2rem",
+                            }}
+                        contentArrowStyle={{
+                            borderRight: '0.4rem solid #9ca3af',
+                        }}  
+                        date={item.date}
+                        icon={item.icon}
+                        iconStyle={{
+                            background:"white",
+                            fontSize:"1.5rem",
+                        }}
                         >
-                            <h3 className="vertical-timeline-element-title">{item.title}</h3>
-                            <p className="vertical-timeline-element-subtitle">{item.location}</p>
-                            <p>{item.description}</p>
+                            <h3 className=" font-semibold capitalize ">{item.title}</h3>
+                            <p className=" font-normal !mt-0 ">{item.location}</p>
+                            <p className='!mt-1 !font-normal text-gray-700'>{item.description}</p>
                         </VerticalTimelineElement>
                     </React.Fragment>
                 ))
