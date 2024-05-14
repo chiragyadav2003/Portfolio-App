02cd0f2f3b8001be1899e8972225cad79d872437
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-14 17:17:20 +0530

    project update

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,55 +2,55 @@
 import { useRef } from 'react'
 import { projectsData } from '@/lib/data'
 import Image from 'next/image'
-import {  motion, useScroll, useTransform } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
 
 
 //**imp */
 type projectProps = (typeof projectsData)[number]
 
-function Project({title,description,imageUrl,link,tags}:projectProps) {
+function Project({ title, description, imageUrl, link, tags }: projectProps) {
     const ref = useRef<HTMLDivElement>(null)
-    const {scrollYProgress}=useScroll({
+    const { scrollYProgress } = useScroll({
         target: ref,
-        offset:["0 1", "1.33 1"]
+        offset: ["0 1", "1.33 1"]
     })
-    const scaleProgress = useTransform(scrollYProgress,[0,1],[0.5,1])
-    const opacityProgress = useTransform(scrollYProgress,[0,1],[0.5,1])
+    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1])
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1])
     return (
         <motion.div ref={ref}
-        style={{
-            scale:scaleProgress,
-            opacity:opacityProgress
-        }}
-        className='group mb-3 sm:mb-8 last:mb-0'
+            style={{
+                scale: scaleProgress,
+                opacity: opacityProgress
+            }}
+            className='group mb-3 sm:mb-8 last:mb-0'
         >
-            <section 
-            className="bg-gray-100 max-w-[42rem] borderBlack rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-            
-            <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-                <a href={link} target='_blank'>
-                    <h3 className="text-2xl font-semibold">{title}</h3>
-                </a>
-                <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
-                {description}
-                </p>
-                <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-                {tags.map((tag, index) => (
-                <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                key={index}
-                >
-                {tag}
-                </li>
-            ))}
-                </ul>
-            </div>
+            <section
+                className="bg-gray-100 max-w-[42rem] borderBlack rounded-lg overflow-hidden sm:pr-8 relative sm:h-[25rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
 
-            <Image
-                src={imageUrl}
-                alt="Project I worked on"
-                quality={95}
-                className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+                <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+                    <a href={link} target='_blank'>
+                        <h3 className="text-2xl font-semibold">{title}</h3>
+                    </a>
+                    <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+                        {description}
+                    </p>
+                    <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+                        {tags.map((tag, index) => (
+                            <li
+                                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                                key={index}
+                            >
+                                {tag}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <Image
+                    src={imageUrl}
+                    alt="Project I worked on"
+                    quality={95}
+                    className="absolute hidden sm:block top-8 -right-40 w-[30rem] rounded-t-lg shadow-2xl
                 transition 
                 group-hover:scale-[1.04]
                 group-hover:-translate-x-3
@@ -62,7 +62,7 @@ function Project({title,description,imageUrl,link,tags}:projectProps) {
                 group-even:group-hover:rotate-2
 
                 group-even:right-[initial] group-even:-left-40"
-            />
+                />
             </section>
         </motion.div>
     )
--- lib/data.ts
+++ lib/data.ts
@@ -1,70 +1,88 @@
 import React from "react";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import Blog from "../public/blog.png"
-import compiler from "../public/compiler.jpg"
+import Blog from "../public/blog.png";
+import compiler from "../public/compiler.jpg";
+import BudgetTracker from "../public/BudgetTracker.jpg";
+import paytm from "../public/paytm.jpg"
 
 export const links = [
     {
-    name: "Home",
-    hash: "#home",
+        name: "Home",
+        hash: "#home",
     },
     {
-    name: "About",
-    hash: "#about",
+        name: "About",
+        hash: "#about",
     },
     {
-    name: "Projects",
-    hash: "#projects",
+        name: "Projects",
+        hash: "#projects",
     },
     {
-    name: "Skills",
-    hash: "#skills",
+        name: "Skills",
+        hash: "#skills",
     },
     {
-    name: "Experience",
-    hash: "#experience",
+        name: "Experience",
+        hash: "#experience",
     },
     {
-    name: "Contact",
-    hash: "#contact",
+        name: "Contact",
+        hash: "#contact",
     },
 ] as const;
 
 export const experiencesData = [
     {
-    title: "Graduated bootcamp",
-    location: "Delhi, India",
-    description:"I graduated after 6 months of studying.",
-    icon: React.createElement(LuGraduationCap), 
-    //**we are using ts file so we can not use jsx/tsx syntax here so we are using React.createElement
-    date: "2024",
+        title: "Graduated bootcamp",
+        location: "Delhi, India",
+        description: "I graduated after 6 months of studying.",
+        icon: React.createElement(LuGraduationCap),
+        //**we are using ts file so we can not use jsx/tsx syntax here so we are using React.createElement
+        date: "2024",
     },
     {
-    title: "Full-Stack Developer",
-    location: "Delhi, India",
-    description:"I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma ORM, Connection Pooling, Serverless(CLoudFlare-Workers) and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2024 - present",
+        title: "Full-Stack Developer",
+        location: "Delhi, India",
+        description: "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma ORM, Connection Pooling, Serverless(CLoudFlare-Workers) and MongoDB. I'm open to full-time opportunities.",
+        icon: React.createElement(FaReact),
+        date: "2024 - present",
     },
 ] as const;
 
 export const projectsData = [
     {
+        title: "BudgetTracker",
+        description:
+            "BudgetTracker is a cutting-edge end-to-end budget management application built using a robust technology stack and a variety of other innovative tools.",
+        tags: ["TypeScript", "React.js", "Next.js", "PostgreSQL", "Prisma", "tailwind CSS", "TanStack Query", "CLerk Auth", "Zod Validation", "ReCharts"],
+        imageUrl: BudgetTracker,
+        link: "https://budget-tracker-chirag-yadavs-projects.vercel.app/"
+    },
+    {
+        title: "Paytm",
+        description:
+            "This project is a solo endeavor showcasing a web application inspired by digital payment platforms. Users can create accounts, transfer funds, and send money securely.",
+        tags: ["Node.js", "Docker", "AWS", "Tailwind", "Typescript", "Prisma ORM", "Postgres", "Next.js", "CI-CD pipeline", "Turborepo", "Recoil", "Bcrypt", "NextAuth"],
+        imageUrl: paytm,
+        link: "https://github.com/chiragyadav2003/Paytm"
+    },
+    {
         title: "DevBlog",
         description:
-        "A blogging app where user can create, update, read and delete blogs. It has a user authentication system and a dashboard for the user to manage their blogs.",
-        tags: ["React", "ClouFlare-Workere", "Zod", "Tailwind","Typescript","Prisma ORM","Connection Pooling","Postgres","JWT","NPM Publish"],
+            "A blogging app where user can create, update, read and delete blogs. It has a user authentication system and a dashboard for the user to manage their blogs.",
+        tags: ["React", "ClouFlare-Workere", "Zod", "Tailwind", "Typescript", "Prisma ORM", "Connection Pooling", "Postgres", "JWT", "NPM Publish"],
         imageUrl: Blog,
-        link:"https://dev-blog-alpha-ten.vercel.app/"
+        link: "https://dev-blog-alpha-ten.vercel.app/"
     },
     {
         title: "CodePen",
         description:
-        "CodePen is an APP for testing and showcasing user-created HTML, CSS and JavaScript code snippets. It functions as an online code editor where developers can create code snippets, and test them.",
-        tags: ["React","codemirror","material-ui","react-codemirror"],
+            "CodePen is an APP for testing and showcasing user-created HTML, CSS and JavaScript code snippets. It functions as an online code editor where developers can create code snippets, and test them.",
+        tags: ["React", "codemirror", "material-ui", "react-codemirror"],
         imageUrl: compiler,
-        link:"https://codepen-clone-lime.vercel.app/",
+        link: "https://codepen-clone-lime.vercel.app/",
     }
 ] as const;
 
@@ -73,27 +91,32 @@ export const skillsData = [
     "CSS",
     "JavaScript",
     "TypeScript",
-    "React",
+    "Hono.js",
+    "React.js",
     "Next.js",
+    "Docker",
+    "Prisma",
     "Node.js",
     "Git",
+    "GitHub",
     "Tailwind",
     "Prisma",
+    "Connection Pooling",
     "MongoDB",
-    "Redux",
-    "Express",
     "PostgreSQL",
+    "MySQL",
+    "Redux",
+    "Recoil",
+    "Zustand",
+    "Express",
     "Framer Motion",
     "Appwrite",
+    "Shadcn UI",
+    "Radix UI",
     "Material-UI",
     "Chakra-UI",
     "Bootstrap",
+    "Serverless(Cloudflare workers)",
     "Jest",
-    "React-Query",
-    "Netlify",
-    "Vercel",
-    "Windows",
-    "C",
-    "C++",
-    "Java",
+    "tenStack-Query",
 ] as const;
